623c764cf888096ecf0a2cf4c07a1abd7343a87e
QuentO <qochieng@gmail.com>
2019-02-03 13:45:47 -0800

    Changed Collate
    Changed collate type  to match PostgreSQL install.

--- create-spaceman-db.sql
+++ create-spaceman-db.sql
@@ -18,8 +18,8 @@ CREATE DATABASE spaceman_game
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
+    LC_COLLATE = 'English_United States.1252' -- Formerly 'C'
+    LC_CTYPE = 'English_United States.1252' -- Formerly 'C'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
